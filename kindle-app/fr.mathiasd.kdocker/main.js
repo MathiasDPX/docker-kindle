83c3ed772ed762c4b06153fc4ad6552aeb109c26
MathiasDPX <mathias@dupeux.net>
2026-01-25 03:54:10 +0100

    feat(app): add containers

--- kindle-app/fr.mathiasd.kdocker/main.js
+++ kindle-app/fr.mathiasd.kdocker/main.js
@@ -2,4 +2,7 @@ if (!navigator.userAgent.includes("Kindle")) {
     document.body.style.height = "800px";
     document.body.style.width = "480px";
     document.body.style.border = "2px solid #333";
+    document.body.style.margin = "1em";
+
+    // todo: add github link under body (visible cuz we're not on kindle)
 }
