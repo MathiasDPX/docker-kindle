310324bb1a17f5976411b7a45e539bd2c35d8cd1
MathiasDPX <mathias@dupeux.net>
2026-01-25 02:19:17 +0100

    feat(api): sort containers by state

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"context"
+	"math"
 	"net/http"
+	"sort"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/moby/moby/api/types/container"
 	"github.com/moby/moby/client"
 )
+
+// Weight in the sort
+var RANKS = map[string]int{
+	"dead":       0,
+	"exited":     1,
+	"paused":     2,
+	"restarting": 3,
+	"running":    4,
+	"created":    5,
+}
+
+func rank(state container.ContainerState) int {
+	s := strings.ToLower(string(state))
+
+	if r, ok := RANKS[s]; ok {
+		return r
+	}
+	return math.MaxInt
+}
 
 func main() {
 	cli, err := client.New(
@@ -27,6 +50,10 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+
+		sort.Slice(containers.Items, func(i, j int) bool {
+			return rank(containers.Items[i].State) < rank(containers.Items[j].State)
+		})
 
 		result := []gin.H{}
 		for _, c := range containers.Items {
