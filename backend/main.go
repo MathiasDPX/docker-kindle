e9e062687a737bed7fd05721c74942686d9c8e6f
MathiasDPX <mathias@dupeux.net>
2026-01-25 01:50:03 +0100

    feat(api): add index route

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/moby/moby/client"
 )
 
-func Index(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
-}
-
 func main() {
+	cli, err := client.New(
+		client.FromEnv,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 
-	r.GET("/", Index)
+	r.GET("/", func(ctx *gin.Context) {
+		containers, err := cli.ContainerList(context.Background(), client.ContainerListOptions{
+			All: true,
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		var result []gin.H
+		for _, c := range containers.Items {
+			result = append(result, gin.H{
+				"id":     c.ID[:12],
+				"name":   c.Names[0][1:],
+				"state":  c.State,
+				"status": c.Status,
+			})
+		}
+
+		ctx.JSON(http.StatusOK, result)
+	})
 
 	r.Run()
 }
