e6312a7494d12e13f1cae2f2852c1b8bd5b81a8c
MathiasDPX <mathias@dupeux.net>
2026-01-25 03:06:30 +0100

    style(app): add font

---
+++ kindle-app/fr.mathiasd.kdocker/main.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes("Kindle")) {
+    document.body.style.height = "800px";
+    document.body.style.width = "480px";
+    document.body.style.border = "2px solid #333";
+}
