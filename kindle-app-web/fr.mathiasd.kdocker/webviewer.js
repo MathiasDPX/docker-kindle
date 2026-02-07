3a92c885db6ec13e8023d3c816e297c368841009
MathiasDPX <mathias@dupeux.net>
2026-02-07 09:35:26 +0100

    minor(web): add repo link under viewport

--- kindle-app-web/fr.mathiasd.kdocker/webviewer.js
+++ kindle-app-web/fr.mathiasd.kdocker/webviewer.js
@@ -4,5 +4,16 @@ if (!(navigator.userAgent.includes("Kindle") || navigator.userAgent.includes("Si
     document.body.style.border = "2px solid #333"
     document.body.style.margin = "1em"
 
-    // todo: add github link under body (visible cuz we're not on kindle)
+    repo = document.createElement("a");
+    repo.href = "https://github.com/MathiasDPX/docker-kindle";
+    repo.innerText = "MathiasDPX/docker-kindle";
+
+    repo.style['text-align'] = "center";
+    repo.style['width'] = "100%";
+    repo.style['display'] = "block";
+    repo.style['padding-top'] = "1.5em";
+    repo.style['color'] = "#0D65EF";
+    repo.style['text-decoration'] = "underline";
+
+    document.body.insertAdjacentElement("beforeend", repo)
 }
